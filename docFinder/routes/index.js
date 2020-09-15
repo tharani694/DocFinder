83cff9c9da3c7d6a4b6abfb7c8af05aca28210d3
Tharani Malikha <53375286+tharani694@users.noreply.github.com>
2020-09-15 11:52:32 +0530

    Update index.js

--- docFinder/routes/index.js
+++ docFinder/routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
+//home page for docFinder
 router.get('/', function(req, res, next) {
   res.render('index');
 });
